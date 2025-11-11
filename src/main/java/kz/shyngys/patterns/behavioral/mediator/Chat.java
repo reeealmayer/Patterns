34b342cd78d5de4f1689eda697098bcbd5a471ef
rilmayer <yessenbayev.chingis@gmail.com>
2025-11-11 06:54:15 +0500

    PATTERNS: Mediator, Memento

---
+++ src/main/java/kz/shyngys/patterns/behavioral/mediator/Chat.java
@@ -0,0 +1,5 @@
+package kz.shyngys.patterns.behavioral.mediator;
+
+public interface Chat {
+    void sendMessage(String message, User user);
+}
---
+++ src/main/java/kz/shyngys/patterns/behavioral/mediator/User.java
@@ -0,0 +1,7 @@
+package kz.shyngys.patterns.behavioral.mediator;
+
+public interface User {
+    void sendMessage(String message);
+
+    void getMessage(String message);
+}
